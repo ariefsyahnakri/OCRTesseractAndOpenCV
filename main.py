ba8c79fda723d5dedea349fa7968b8367a5d5439
ariefsyahnakri <m.ariefsyahnakri@gmail.com>
2021-11-29 10:32:11 +0700

    adding libraries in main.py and code structure

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import pytesseract
+import cv2 as cv
+
+
+tesseractFile = "C:\Program Files\Tesseract-OCR\Tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = tesseractFile
+
+# Inisasi file citra
+
+
+# Color2Gray process
+
+
+# Unsharp masking process
+
+
+# Otsu thresholding process
+
+
+# Tesseract process
 
